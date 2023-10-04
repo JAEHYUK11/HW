c81f4aafe9373ea94870aa6ae7235d4318fe192e
JAEHYUK SIM <wogur0238@gmail.com>
2023-10-04 17:07:59 +0900

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,30 +1,38 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int N = 2, M = 3, NM;
-	float A[2][3] = { {1,2,3},{4,5,6} }, ** B, ** C;
-	B = new float* [M];
-	for (int n = 0; n < M; n++) B[n] = new float[N];
-	C = new float* [N];
-	for (int n = 0; n < N; n++) C[n] = new float[N];
-	
-	B[0][0] = 1; B[0][1] = 0;
-	B[1][0] = 2; B[1][1] = 2;
-	B[2][0] = 0; B[2][1] = 2; 
+    int N = 2, M = 3, NM;
+    float A[2][3] = { {1,2,3},{4,5,6} }, ** B, ** C;
+    B = new float* [M];
+    for (int n = 0; n < M; n++) B[n] = new float[N];
+    C = new float* [N];
+    for (int n = 0; n < N; n++) C[n] = new float[N];
 
-	// C = A * B
-	for (int n = 0; n < N; n++) {
-		for (int l = 0; l < N; ++l) {
-			for (int m = 0; m < M; m++) {
-				C[n][l] += A[n][m] * B[m][l];
-			}
-		}
-	}
-	// show results
-	for (int n = 0; n < N; n++) {
-		for (int l = 0; l < N; l++) {
-			cout << C[n][l] << "  ";
-		}
-		cout << endl;
-	}
+    B[0][0] = 1; B[0][1] = 0;
+    B[1][0] = 2; B[1][1] = 2;
+    B[2][0] = 0; B[2][1] = 2;
+    for (int n = 0; n < 2; ++n) {
+        for (int l = 0; l < 2; ++l) {
+            C[n][l] = 0;
+        }
+    }
+    // C = A * B
+    for (int n = 0; n < N; n++) {
+        for (int l = 0; l < N; ++l) {
+            for (int m = 0; m < M; m++) {
+                C[n][l] += A[n][m] * B[m][l];
+            }
+        }
+    }
+    // show results
+    for (int n = 0; n < N; n++) {
+        for (int l = 0; l < N; l++) {
+            cout << C[n][l] << "  ";
+        }
+        cout << endl;
+    }
+    delete[] B;
+    delete[] C;
+	
+    return 0;
 }
